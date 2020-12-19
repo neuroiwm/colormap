04eafce11f6b30d5807d59e0e9b58badbcfd123f
neuroIwm <student@pasokon-3.local>
2020-12-19 21:56:16 +0900

    butterfry

---
+++ IWM/2020-12-19/demo.m
@@ -0,0 +1,31 @@
+Fs  = 1000;
+N   = 100;
+t   = 1/Fs : 1/Fs : 1;
+f1  = 2;
+f2  = 10;
+sig = [cos(2*pi*f1*(repmat(t,[N,1])+pi*randn(N,1))).*sin(2*pi*f2*(repmat(t,[N,1])+pi*randn(N,1)))]';
+sig = sig.*rand(1,size(sig,2));
+
+%
+%% conventional
+figure;
+a= plot(t,sig,'LineWidth',1);
+%%
+close all
+try
+    fig;
+    setFig2(4,12);
+    setPos_ppt(1);
+    setLabel('Time [s]','test');
+catch
+    figure;
+end
+ylim([-1.5 1.5]);
+[struct_line,func] = plot_bt(t,sig);
+for i = 1 : N
+    func(struct_line,i);
+    pause(0.1)
+end
+%%
+
+
---
+++ IWM/2020-12-19/plot_bt.m
@@ -0,0 +1,27 @@
+function [struct_line,func] = plot_bt(x,y)
+gr          = [0.8 0.8 0.8]; 
+num_line    = size(y,2);
+struct_line = struct;
+lines       = plot(x,y);
+colors_orig = cat(1,lines(:).Color);
+struct_line.color = colors_orig;
+struct_line.line  = lines;
+struct_line.gr    = gr;
+struct_line.num_line = num_line;
+struct_line.objList  =1:num_line;
+func = @changeLineCol;
+    function struct_line=changeLineCol(struct_line,linenum)
+        for i = 1 : struct_line.num_line
+        struct_line.line(i).Color     = struct_line.gr;
+        struct_line.line(i).LineWidth = 1;
+        end
+        objList   = struct_line.objList;
+        struct_line.line(objList == linenum).Color = struct_line.color(objList == linenum,:);
+        struct_line.line(objList == linenum).LineWidth = 2;
+        oblist  = struct_line.line;
+        newh    = [oblist(objList == linenum);oblist(objList ~= linenum)];
+        set(gca,'Children',newh);
+        struct_line.objList = [objList(objList == linenum),objList(objList ~= linenum)];
+        struct_line.line = newh;
+    end
+end
